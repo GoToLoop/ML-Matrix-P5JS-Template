68dfaae140a7706cb1e801f615c5bf4c91fe122b
GoToLoop <emlf@hotmail.com>
2021-09-05 06:01:48 -0300

    fix parameter matrix

--- sketch.ts
+++ sketch.ts
@@ -1,5 +1,5 @@
 /**
- * ML-Matrix + p5js Template (v1.0.0)
+ * ML-Matrix + p5js Template (v1.0.1)
  * GoToLoop (2021-Sep-04)
  *
  * https://Discourse.Processing.org/t/autocomplete-and-documentation-for-imported-p5js/31927/14
